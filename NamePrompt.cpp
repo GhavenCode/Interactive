391838b8bab067f9ac912f19014450cc0a75ec57
Stewart98 <97505661+Stewart98@users.noreply.github.com>
2022-01-26 10:01:09 +0200

    Update NamePrompt.cpp
    body

--- NamePrompt.cpp
+++ NamePrompt.cpp
@@ -1,1 +1,27 @@
 
+#include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+  
+  
+  
+  
+  
+  
+    system("pause");
+    return 0;
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
